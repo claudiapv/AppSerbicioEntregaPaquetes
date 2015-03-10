9334603cb10b0210f92bd1e6eba2d0733c0c5822
claupapuva <claupapuva@gmail.com>
2015-03-09 19:36:30 -0500

    update

---
+++ App/Modelo/OvernightPackage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class OvernightPackage ()
+    {
+        #region "Atributos"
+        string CostoEnvioNoche;
+        #endregion
+
+        #region "Propiedades"
+        public string CostoEnvioNoche1
+        {
+            get { return CostoEnvioNoche; }
+            set { CostoEnvioNoche = value; }
+        }
+        #endregion
+
+        #region "Constructor"
+        public OvernightPackage ()
+         {
+            this.CostoEnvioNoche = "4000";
+         }
+        #endregion
+        public OvernightPackage(string CostoEnvioNoche)
+         {
+             this.CostoEnvioNoche = "4000";
+
+
+
+         }
+    }
+}
---
+++ App/Modelo/Package.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Package
+    {
+        #region "Atributos" 
+        string Codigo;
+        string Nombre;
+        string Dirección;
+        string Ciudad;
+        string Departamento;
+        string CodPosR;
+        string CodPosD;
+        string PesoPaq;
+        string Costo;
+        #endregion
+
+        #region "Propiedades"
+        public string Codigo1
+        {
+            get { return Codigo; }
+            set { Codigo = value; }
+        }
+        public string Nombre1
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        public string Dirección1
+        {
+            get { return Dirección; }
+            set { Dirección = value; }
+        }
+        public string Ciudad1
+        {
+            get { return Ciudad; }
+            set { Ciudad = value; }
+        }
+        public string Departamento1
+        {
+            get { return Departamento; }
+            set { Departamento = value; }
+        }
+        public string CodPosR1
+        {
+            get { return CodPosR; }
+            set { CodPosR = value; }
+        }
+        public string CodPosD1
+        {
+            get { return CodPosD; }
+            set { CodPosD = value; }
+        }
+        public string PesoPaq1
+        {
+            get { return PesoPaq; }
+            set { PesoPaq = value; }
+        }
+        public string Costo1
+        {
+            get { return Costo; }
+            set { Costo = value; }
+        }
+        #endregion
+
+
+        #region "Constructor"
+        public Package()
+         {
+             this.Codigo = "123";
+             this.Nombre = "Caroline Castañeda";
+             this.Dirección = "San Fernando";
+             this.Ciudad = "Cartagena";
+             this.Departamento = "Bolivar";
+             this.CodPosR = "1234";
+             this.CodPosD = "589";
+             this.PesoPaq = "15";
+             this.Costo = "2000";
+         }
+        public Package (string Codigo, string Nombre, string Dirección, string Ciudad, string Departamento, string CodPosR, string CodPosD, string PesoPaq, string Costo) 
+
+             {
+                 this.Codigo = Codigo;
+                 this.Nombre = Nombre;
+                 this.Dirección = Dirección;
+                 this.Ciudad = Ciudad;
+                 this.Departamento = Departamento;
+                 this.CodPosD = CodPosD;
+                 this.CodPosR = CodPosR;
+                 this.PesoPaq = PesoPaq;
+                 this.Costo = Costo;
+
+             }
+               
+        #endregion
+
+     
+
+       }
+    
+}
